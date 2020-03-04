9807e0fcd16ec3afefa26dcce3ab20cadd66d980
PolyStu20 <cepnz@kinect.co.nz>
2020-03-05 09:50:33 +1300

    Added JFrame and screen size settings

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,8 +1,17 @@
-
-public class Pong {
-
+import javax.swing.JFrame;
+// as this needs a graphical interface, we import the JFram and add " extends JFrame" to the class.
+public class Pong extends JFrame {
+	// sets up the display window
+	public Pong() {
+    	setTitle("Pong"); //this displays on the title of the screen
+    	setSize(800, 600); //sets the size of the screen
+    	setResizable(false); // set this to true if you want the screen to be able to be resized
+    	setVisible(true); // set to true to be visible
+    	setDefaultCloseOperation(EXIT_ON_CLOSE);
+}			// Other options are available, such as DO_NOTHING_ON_CLOSE and HIDE_ON_CLOSE if there are multiple screens
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		new Pong(); // creates the new class Pong
 
 	}
 
