1e3f6648e990e697c4bb730c8970ba1a6e1d71f8
PolyStu20 <cepnz@kinect.co.nz>
2020-03-05 09:59:10 +1300

    added final static variables for the window on Pong class

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,10 +1,14 @@
 import javax.swing.JFrame;
 // as this needs a graphical interface, we import the JFram and add " extends JFrame" to the class.
 public class Pong extends JFrame {
+	// creates final static variables for the window:
+	private final static String WINDOW_TITLE = "Pong"; //creates permanent variables so they can be referred to and changed in only one place
+	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
 	// sets up the display window
 	public Pong() {
-    	setTitle("Pong"); //this displays on the title of the screen
-    	setSize(800, 600); //sets the size of the screen
+    	setTitle(WINDOW_TITLE); //this displays on the title of the screen
+    	setSize(WINDOW_WIDTH, WINDOW_HEIGHT); //sets the size of the screen
     	setResizable(false); // set this to true if you want the screen to be able to be resized
     	setVisible(true); // set to true to be visible
     	setDefaultCloseOperation(EXIT_ON_CLOSE);
