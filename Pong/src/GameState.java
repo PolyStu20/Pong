5992ea7fa03cb7cd29cc34a8461500a3f614c88c
PolyStu20 <cepnz@kinect.co.nz>
2020-03-09 10:31:03 +1300

    created ENUM Gamestate

---
+++ Pong/src/GameState.java
@@ -0,0 +1,8 @@
+//Created a new enumeration called 'GameState' with the options 'Initialising', 'Playing', and 'GameOver'. 
+//We may add more in the future, but we know these are going to be used for our application.
+
+public enum GameState {
+	Initialising,
+	Playing,
+	GameOver,
+}
